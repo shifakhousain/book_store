13b842c68ce2c1d79773da72950e3d3aca463c5d
shifakhousain <shifakhousain@gmail.com>
2024-10-09 11:02:48 +0530

    9th oct 2024

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.urls import reverse
 from django.utils.text import slugify
+from django.db import models
+
+class Member(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
 
 class Book(models.Model):
     title = models.CharField(max_length=50)
@@ -10,7 +16,7 @@ class Book(models.Model):
     )
     author = models.CharField(null=True, max_length=100)
     is_bestselling = models.BooleanField(default=False)
-    slug = models.SlugField(default="", null=False, unique=True)
+    slug = models.SlugField(default="", blank=True, editable=False, null=False, unique=True)
 
     def get_absolute_url(self):
         return reverse("book_detail", args=[self.id])
