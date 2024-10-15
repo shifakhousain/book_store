11dceda94c6b8be3c5d02468e0e791f06d5f8a83
shifakhousain <shifakhousain@gmail.com>
2024-10-15 13:09:26 +0530

    15th oct 2024

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -4,6 +4,15 @@ from django.urls import reverse
 from django.utils.text import slugify
 from django.db import models
 
+# Create your models here.
+class Author(models.Model):  # Inherit from models.Model
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
 class Member(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
